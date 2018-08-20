02baf5f9a1d9e57d622cd18792cedb931b3edb7f
Dreyc Hawking <Dreycoon@gmail.com>
2018-08-19 22:30:12 -0700

    init commit

---
+++ Assets/CameraController.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour {
+	// Use this for initialization
+	bool toggleCamera;
+
+	void Start () {
+			this.GetComponent<ObjectFollowObject>().enabled = true;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(Input.GetButtonDown("Hook")){
+			this.GetComponent<ObjectFollowObject>().enabled = !this.GetComponent<ObjectFollowObject>().enabled;
+			this.GetComponent<AimingCamera>().enabled = !this.GetComponent<AimingCamera>().enabled;
+		}
+	}
+}
---
+++ Assets/ObjectFollowObject.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObjectFollowObject : MonoBehaviour {
+
+	public Transform target;
+	static float smoothSpeed = 10f;
+	static float cameraTurnSpeed = 2f;
+	public Vector3 offset;
+	public Transform lookAtTarget;
+
+	// Use this for initialization
+	void Start () {
+		// pass through follow target
+		Physics.IgnoreCollision(target.GetComponent<Collider>(), GetComponent<Collider>());
+	}
+	
+	// Update is called once per frame
+	void LateUpdate () {
+		// add camera rotation to the offset
+		offset = Quaternion.AngleAxis(Input.GetAxis("Mouse X") * cameraTurnSpeed, Vector3.up) * offset;
+		offset = Quaternion.AngleAxis(Input.GetAxis("Mouse Y") * cameraTurnSpeed, Vector3.right) * offset;
+
+		// don't clip through things
+		var desiredPos = target.position + offset;
+		desiredPos.y  = desiredPos.y < 0 ? 0 : desiredPos.y;
+
+		// add some lag time to the camera
+		var smoothPos = Vector3.Lerp(transform.position, desiredPos, smoothSpeed * Time.deltaTime);
+
+		transform.position = smoothPos;
+		transform.LookAt(lookAtTarget);
+	}
+}
